cf790c63a2982d492bc2ffdfc941a385a956688a
okdtr <okada.teru@gmail.com>
2020-03-03 03:00:10 +0800

    ficed context indentation

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -4,17 +4,18 @@ from django import forms
 
 
 class SignUpForm(UserCreationForm):
-  email = forms.EmailField()
-  first_name = forms.CharField(max_length = 100)
-  last_name = forms.CharField(max_length = 100)
-  fav_color = forms.CharField(max_length = 100)
+  email = forms.EmailField(widget=forms.TextInput(attrs={'class':'form-control'}))
+  first_name = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control'}))
+  last_name = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control'}))
+  fav_color = forms.CharField(max_length = 100, widget=forms.TextInput(attrs={'class':'form-control'}))
   
   class Meta:
       model = User
       fields = ('username','first_name','last_name','email','password1','password2','fav_color')
 
+  def __init__(self, *args, **kwargs):
+    super(SignUpForm, self).__init__(*args, **kwargs)
 
-# class ProfileForm(forms.ModelForm):
-#   class Meta:
-#       model = Profile
-#       fields = ('bio', 'location', 'birth_date')
+    self.fields['username'].widget.attrs['class'] = 'form-control'
+    self.fields['password1'].widget.attrs['class'] = 'form-control'
+    self.fields['password2'].widget.attrs['class'] = 'form-control'
